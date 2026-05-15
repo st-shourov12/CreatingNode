9b4c171495f44df6706a11e03667c20120c72703
st-shourov12 <shourovsc38@gmail.com>
2026-05-15 22:44:16 +0600

    get single product

--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -1,9 +1,17 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
+import type { IProduct } from "../types/product.type";
 
 export const productController = (req :IncomingMessage, res: ServerResponse) => {
     const url = req.url;
     const method = req.method;
+
+    const urlParts = url?.split("/");
+    // console.log(urlParts);
+    const id = urlParts && urlParts[1] === 'products' ? Number(urlParts[2]) : null;
+    // console.log(id);
+
+    // Get all Product
     if (url === "/products"  && method === "GET") {
         // const products = [
         //     {
@@ -16,5 +24,17 @@ export const productController = (req :IncomingMessage, res: ServerResponse) => 
 
         res.writeHead(200,{"content-type" : "application/json"})
         res.end(JSON.stringify({message : "This is products route", data : products}))
+    } else if (method === "GET" && id !== null) {
+        const products = readProduct();
+        const product = products.find((p:IProduct) => p.id === id);
+        console.log(product);
+        res.writeHead(200,{"content-type" : "application/json"})
+        res.end(
+            JSON.stringify({
+                message : "Product Retrieve Successfully", 
+                data : product
+            })
+        )
+        
     }
 }
--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import fs  from "fs"
 
-const filePath = path.join(process.cwd(), './src/database/db.json')
+const filePath = path.join(process.cwd(), "./src/database/db.json")
+
 
 export const readProduct = () => {
-    const products = fs.readFileSync(filePath, 'utf-8');
-    console.log(JSON.parse(products));
+    const products = fs.readFileSync(filePath , 'utf-8')
+    console.log(products);
     return JSON.parse(products)
 }
